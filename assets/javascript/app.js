c0c6c9575cae5eeac8a45fd4db29090619e558bf
Davis Cabrera <davis.cabrera@gmail.com>
2019-05-10 02:09:54 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,8 @@
+$('#fullpage').fullpage({
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    controlArrows: false
+    // more options here
+})
 
-
+alert("fu")
